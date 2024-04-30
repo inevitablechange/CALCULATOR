d5f05360cf139c2abe07da48de5be34f31a4bff4
inevitablechange <yongho7783@gmail.com>
2024-04-30 14:45:05 +0900

    onClickOperator

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const resultEl = document.querySelector(".result");
 const form = document.querySelector("form");
 
 let result = 0;
+let operator = "";
 
 resultEl.innerHTML = result;
 
@@ -18,3 +19,24 @@ function onClickNumber(number) {
   // arr.push(num);
   // resultEl.innerHTML = parseInt(arr.join(""));
 }
+
+function onClickOperator(oprt) {
+  /*
+  문자를 숫자로 바꾸는 방법
+  1. parseInt(문자)
+  2. +문자
+  3. Number(문자) 
+  */
+  result = +resultEl.innerHTML;
+
+  resultEl.innerHTML = 0;
+
+  operator = oprt;
+}
+
+// // enter를 클릭하면 새로운 입력값 받고
+// // 계산하고
+// // 결과물 보여줌
+// function onClickEnter() {
+//   if ()
+// }
