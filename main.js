e2782d130496aeccabfac7bce480534aceaee82a
inevitablechange <yongho7783@gmail.com>
2024-04-30 13:49:46 +0900

    clickNumber

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+const resultEl = document.querySelector(".result");
+const form = document.querySelector("form");
+
 let result = 0;
 
-const resultEl = document.querySelector(".result");
-
 resultEl.innerHTML = result;
+
+function onClickNumber(number) {
+  //return은 값을 반환하는 동시에 함수를 끝낸다.
+  // 함수블록 안에 return만 있는 경우 {}는 없애도 된다.
+  if (resultEl.innerHTML.length > 12) return;
+
+  if (resultEl.innerHTML === "0") {
+    resultEl.innerHTML = number;
+  } else {
+    resultEl.innerHTML += number;
+  }
+  // arr.push(num);
+  // resultEl.innerHTML = parseInt(arr.join(""));
+}
